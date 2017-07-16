ddee8e677576961a88d22f61c7b7dc81bf611645
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-07-16 10:51:20 -0400

    Adds redux-devtools-extension support.

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { default as reducers } from 'reducers/index';
 import { getInitialState as getSearchInitialState } from 'reducers/search.reducer';
 import classToPlainObject from 'middlewares/classToPlainObject';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import type { SearchStateType } from 'reducers/search.reducer';
 
@@ -10,12 +11,14 @@ export type ApplicationState = {
   search: SearchStateType;
 };
 
-export const getApplicationInitialState = () => {
+export const getApplicationInitialState = () : ApplicationState => {
   return Object.assign({}, {
     search: getSearchInitialState()
   });
 };
 
-export const configureStore = (initialState: ApplicationState = getApplicationInitialState()) => {
-  return createStore(reducers, initialState, applyMiddleware(classToPlainObject));
+export const configureStore = (initialState: ApplicationState = getApplicationInitialState()) : any => {
+  return createStore(reducers, initialState, composeWithDevTools(
+    applyMiddleware(classToPlainObject)
+  ));
 };
